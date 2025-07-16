7e36cdaee5d56ae68d542ea789a5e499068e4100
Ctrl-Eyob <eyobaberabekru@gmai.com>
2025-07-16 15:58:02 +0300

    message displayed

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,5 @@
+import WelcomeMessage from './components/WelcomeMessage';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,7 +9,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
+    <>function App() {
+  return (
     <>
+      <WelcomeMessage />
+    </>
+  );
+}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
