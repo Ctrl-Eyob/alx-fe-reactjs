5951de71cf6c9b48b45171c64a89b1661b298060
Ctrl-Eyob <eyobaberabekru@gmai.com>
2025-07-20 17:57:23 +0300

    added head,main and footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,7 +5,20 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import WelcomeMessage from './components/WelcomeMessage';
+import React from 'react';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
 function App() {
   const [count, setCount] = useState(0);
 
