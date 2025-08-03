c4e48932cd2c3e689ae03493dbb9c730fc37147a
Ctrl-Eyob <eyobaberabekru@gmai.com>
2025-08-03 15:00:54 +0300

    app aand counter jsx added

---
+++ alx-react-app-new/src/components/App.jsx
@@ -0,0 +1,19 @@
+// src/App.jsx
+import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import Counter from './components/Counter'; // ✅ Import Counter
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Counter /> {/* ✅ Add Counter */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
