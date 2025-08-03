cfec9fdc59cbe48cb3cdb253fac1c5156921ac48
Ctrl-Eyob <eyobaberabekru@gmai.com>
2025-08-03 15:03:01 +0300

    import counter in app.jsx

--- alx-react-app-new/src/components/App.jsx
+++ alx-react-app-new/src/components/App.jsx
@@ -1,4 +1,5 @@
 // src/App.jsx
+import Counter from './components/Counter';
 import React from 'react';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
