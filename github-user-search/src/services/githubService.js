7beaeb53a83b54d607fbf7e19932203aac8de4c9
Ctrl-Eyob <eyobaberabekru@gmail.com>
2026-02-01 11:26:04 +0300

    edited githubservice.js

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,17 +1,42 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BASE_URL = 'https://api.github.com/search/users';
+const githubApi = axios.create({
+  baseURL: "https://api.github.com",
+  headers: {
+    Authorization: import.meta.env.VITE_APP_GITHUB_API_KEY
+      ? `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`
+      : "",
+  },
+});
 
-export async function searchUsers({ username, location, minRepos, page = 1 }) {
-  const queryParts = [];
-
-  if (username) queryParts.push(`${username} in:login`);
-  if (location) queryParts.push(`location:${location}`);
-  if (minRepos) queryParts.push(`repos:>=${minRepos}`);
-
-  const query = queryParts.join(' ');
-  const url = `${BASE_URL}?q=${encodeURIComponent(query)}&page=${page}&per_page=10`;
-
-  const response = await axios.get(url);
+/**
+ * TASK 1 (MANDATORY)
+ * Fetch a single GitHub user by username
+ */
+export const fetchUserData = async (username) => {
+  const response = await githubApi.get(`/users/${username}`);
   return response.data;
-}
+};
+
+/**
+ * TASK 2 (ADVANCED SEARCH)
+ * Search users with filters
+ */
+export const searchUsersAdvanced = async ({
+  username = "",
+  location = "",
+  minRepos = "",
+  page = 1,
+}) => {
+  let query = "";
+
+  if (username) query += username;
+  if (location) query += ` location:${location}`;
+  if (minRepos) query += ` repos:>=${minRepos}`;
+
+  const response = await githubApi.get(
+    `/search/users?q=${query}&page=${page}&per_page=10`
+  );
+
+  return response.data;
+};
